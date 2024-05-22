58c7f038bcb7a62a1173127b1d2f7f912dc07ee8
AlphaPrime512 <josuegamalielgutierrez659@gmail.com>
2024-05-22 16:09:42 -0400

    operadores y condicional

---
+++ app.js
@@ -0,0 +1,68 @@
+//esta es tipo string
+let saludo = 'hola'
+
+//esta es tipo number
+let numero = 9
+
+//esta es tipo booleano
+let verdadero = true
+
+//esta es de tipo undefined
+let indefinido = undefined
+let Indefini2
+
+
+//esta es tipo es null
+let nulo = null
+console.log(nulo)
+
+//operadores aritmeticos: + (suma) - (resta) * (multiplicacion) / (division) % (modulo)
+
+//esta variables es una suma
+let suma = 2 + 5
+
+
+//esta variable es una resta
+let resta = 3 - 3
+
+
+//esta variable es una muktiplicacion
+let multiplicacion = 50 * Number.MAX_SAFE_INTEGER
+
+
+//esta variable es uma división
+let division = 2 / 2
+
+
+//esta variable es un modulo
+let modulo = 2000 % 100
+
+
+//operadores logicos:< (menor que) > (mayor que) <= (menor que) >= (mayor que)
+
+let menorQue = 2 < 1
+
+
+let mayorQue = 2 > 1
+
+let menorOIgualQue = 2 <= 3
+
+let mayorOIgualQue = 1 >= 5
+
+let igual = 2 == 2
+
+//operadpres logicos:Si (&&) y NO (||)
+
+let y =  2 > 1 && 4 > 1
+
+let n = 5 > 10 || 2 < 5
+
+if (2 == 2) {
+ console.log(saludo)    
+}
+
+if (2 == 2) { console.log('es igual.')
+    
+} else { console.log('no es igual')
+    
+}
--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("hola mundo")
+let resta = 1 - 1
+let suma = 250 + 250 
+let multiplicaciones = 3 * 2
+let division = 9 / 1
+console.log(resta)
+console.log(suma)
+console.log(multiplicaciones)
+console.log(division)
+
+console.log(`la suma total es: ` + suma)
